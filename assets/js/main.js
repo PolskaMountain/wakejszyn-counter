cdef62fff8cc3881e39c0fd2a37d9d974801a0f4
Jakub Złotek <kubazlotek@gmail.com>
2021-08-31 21:21:18 +0200

    js formating and string templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,18 @@
-// Set the date we're counting down to
 var countDownDate = new Date("Jun 24, 2022").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
+var x = setInterval(function () {
   var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
+  var percent = (distance / 1000 / 25574400) * 100;
 
-
-  var percent = ((distance / 1000) / 25574400 * 100);
-
-  // if (percent > 100) {
-  //   percent = 100;
-  // }
-
-  document.getElementById("progress").style.width = percent + "%";
-  document.getElementById("progress-text").innerHTML = "Zostało " + percent + "% Roku Szkolnego";
-
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
+  document.getElementById("counter").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  document.getElementById("progress").style.width = `${percent}%`;
+  document.getElementById("progress-text").innerHTML = `Zostało ${percent}% Roku Szkolnego`;
 
 }, 1000);
 
